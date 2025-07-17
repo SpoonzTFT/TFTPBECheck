1543cc16fcce51c2176e53f1b5fb631c7154f5ba
SpoonzTFT <bpoulson14@gmail.com>
2025-07-16 23:10:52 -0600

    Update check.js

--- check.js
+++ check.js
@@ -1,13 +1,13 @@
 const fetch = require("node-fetch");
 
 const API_KEY = process.env.PUSHCUT_API_KEY;
-const PUSHCUT_NOTIFICATION_NAME = "TFT_PBE_Alert";
+const PUSHCUT_NOTIFICATION_ID = "https://api.pushcut.io/KYw8-pAhYoebZ21igemDW/notifications/TFT_PBE_Alert";
 
 (async () => {
   try {
     console.log("Sending test notification...");
 
-    const notify = await fetch(`https://api.pushcut.io/v1/notification/${PUSHCUT_NOTIFICATION_NAME}`, {
+    const notify = await fetch(`https://api.pushcut.io/v1/notifications/${PUSHCUT_NOTIFICATION_ID}`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
